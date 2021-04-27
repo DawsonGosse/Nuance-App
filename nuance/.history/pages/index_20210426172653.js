5537e02f3257275dd453eba2e5ab2d29347bda7b
DawsonGosse <dgosse5@my.bcit.ca>
2021-04-26 21:27:32 -0700

    Dawson & Hannah added hover for nav and animations

---
+++ nuance/.history/pages/index_20210426172653.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import styled from 'styled-components';
+import LogoUI from '../comps/Logo';
+import {useRouter} from 'next/router';
+import ButtonUI from '../comps/Button';
+import BlobBig from '../comps/BlobBig';
+
+const Container = styled.div`
+width:100vw;
+height:100vh;
+display:flex;
+justify-content:flex-end;
+align-items:center;
+flex-direction:column;
+`
+
+const ButtonContainer =styled.div`
+display:flex;
+width:100vw;
+height:200px;
+justify-content:space-evenly;
+align-items:center;
+flex-direction:column;
+margin-top:200px;
+margin-bottom:20px;
+animation-name: FadeIn;
+animation-iteration-count: 1;
+animation-timing-function: ease-in;
+animation-duration: 3s;
+`
+
+export default function LandingPage() {
+  return <Container>
+    <LogoUI></LogoUI>
+    <ButtonContainer>
+      <ButtonUI text='GET STARTED' routeTo='/survey'></ButtonUI>
+      <ButtonUI text='HOW IT WORKS' bgcolor='#fff' color='#3E3D46'></ButtonUI>
+    </ButtonContainer>
+  </Container>
+}
